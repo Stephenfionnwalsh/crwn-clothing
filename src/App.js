09faef9daf25e12d19c736291e5b9409f78e3da1
Stephen Walsh <Stephenfionnwalsh@gmail.com>
2021-11-29 20:52:50 +0100

    add router HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
