37306f19407eede91da8a5c155ada39ac01941ce
Stephen Walsh <Stephenfionnwalsh@gmail.com>
2021-11-29 22:26:34 +0100

    add navigation

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import "./App.css";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import Shop from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/">
             <HomePage />
           </Route>
-          <Route path="/shop">
+          <Route exact path="/shop">
             <Shop />
           </Route>
         </Switch>
